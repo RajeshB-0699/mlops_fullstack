610dc7b988a2078c3d5a73f83705018d72c41a1e
RajeshB-0699 <rajeshbala802@gmail.com>
2025-05-07 08:40:53 +0530

    Added initial requirements

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='src',
+    packages=find_packages(),
+    version='0.1.0',
+    description='mlops project using dagshub and model registry and hyperparameter tuning',
+    author='Rajesh B',
+    license='',
+)
